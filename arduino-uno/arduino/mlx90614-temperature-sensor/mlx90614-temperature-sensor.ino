54103c32cd92171e2abbe73b5861d652f2837cf8
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2021-01-01 21:26:07 +0000

    updated arduino sketch

--- arduino-uno/arduino/mlx90614-temperature-sensor/mlx90614-temperature-sensor.ino
+++ arduino-uno/arduino/mlx90614-temperature-sensor/mlx90614-temperature-sensor.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ * Sketch that reads temperature from a MLX90614 temperature sensor, and prints
+ * the reading to the serial console.
+ */
 
+// Include the libraries we need
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+
+// create sensor object
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+void setup() 
+{
+  Serial.begin(9600);
+
+  Serial.println("** MLX90614 Temperature Reading **"); 
+  mlx.begin();  // initialise sensor
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  // the mlx90614 sensor provides both an ambient temperature reading and an ir
+  // temperature reading of objects in front of the sensor.
+  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC()); Serial.print(" C");
+  Serial.print("\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println(" C");
 
+  Serial.println();
+  delay(1000);
 }
